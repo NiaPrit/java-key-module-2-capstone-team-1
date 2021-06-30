cdca8732ca5d26b28349603157d5029222121ff5
Nia Tucker <ntucker1@kent.edu>
2021-06-30 13:47:10 -0400

    updates

--- Module-2-Capstone/java/tenmo-client/src/main/java/com/techelevator/tenmo/models/account/JDBCAccountDAO.java
+++ Module-2-Capstone/java/tenmo-client/src/main/java/com/techelevator/tenmo/models/account/JDBCAccountDAO.java
@@ -2,7 +2,6 @@ package com.techelevator.tenmo.models.account;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
